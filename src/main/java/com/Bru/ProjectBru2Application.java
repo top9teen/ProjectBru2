81db5fb9507d9ff842b034198e97d7f99a7f2410
top <35331078+top9teen@users.noreply.github.com>
2018-09-25 13:44:50 +0700

    Initial commit

---
+++ src/main/java/com/Bru/ProjectBru2Application.java
@@ -0,0 +1,12 @@
+package com.Bru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBru2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBru2Application.class, args);
+	}
+}
