8ea119cf9017911f5bbe8198a464ecb1db6f9525
top <35331078+top9teen@users.noreply.github.com>
2018-09-28 16:08:08 +0700

    ิิิดกหดหกด
    หกดกหดหกด

--- src/main/java/com/Bru/controller/SearchCarController.java
+++ src/main/java/com/Bru/controller/SearchCarController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-import org.elasticsearch.common.netty.util.internal.DetectionUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
